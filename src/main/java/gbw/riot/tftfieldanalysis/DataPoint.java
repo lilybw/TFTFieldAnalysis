c68ccaad0700c90e65c86f1b134ad487e0ab7402
lilybw <lilybwanscher@gmail.com>
2023-07-06 18:53:02 +0200

    Update DataPoint.java

--- src/main/java/gbw/riot/tftfieldanalysis/DataPoint.java
+++ src/main/java/gbw/riot/tftfieldanalysis/DataPoint.java
@@ -6,11 +6,15 @@ import java.util.HashSet;
 import java.util.List;
 
 public class DataPoint {
-
     private static final List<String> EMPTY = new ArrayList<>();
-
+    private final String namespaceKey;
     private HashMap<String, List<String>> tags;
 
+    public DataPoint(String namespaceKey)
+    {
+        this.namespaceKey = namespaceKey;
+        tags = new HashMap<>();
+    }
 
     public List<String> get(String key){
         return tags == null ? EMPTY : tags.get(key) == null ? EMPTY : tags.get(key);
@@ -21,7 +25,10 @@ public class DataPoint {
     }
 
     public boolean put(String key, String value){
-
+        tags.computeIfAbsent(key, k -> new ArrayList<>());
+        return tags.get(key).add(value);
     }
+
+    
 
 }
